2af7c69b7b586b4596222d4cf35b5dc684cd24ae
hypnotortoise <hypnotortoise@gmail.com>
2019-08-15 18:25:41 +0200

    update contract: - use DAO interface for calling external contract - add state NOT_VOTED - use SafeMath operations - add function checkParticipant() for testing

--- react/contracts/BurnFeePoll.sol
+++ react/contracts/BurnFeePoll.sol
@@ -7,20 +7,24 @@ pragma solidity ^0.5.0;
 */
 import "@openzeppelin-solidity/contracts/math/SafeMath.sol";
 
+interface DigixDao {
+    function isParticipant(address _user) external view returns (bool _is);
+}
+
 contract BurnFeePoll {
     using SafeMath for uint256;
     /**
     * State variables
     */
     address public owner = msg.sender;
-    address constant DGD_Q_CONTRACT = address(0xD7a4f012Ead240691A98144Cb488e483aefeF724);
+    address daoAddress = 0x5d093A0e0328Ad17469b948De7f2DfD4b5eE5544;
+    DigixDao digixDaoContract = DigixDao(daoAddress);
 
-    enum VoteStatus { VOTED, CANCELLED }
+    enum VoteStatus { NOT_VOTED, VOTED, CANCELLED }
 
     struct Vote {
         uint fee;
         VoteStatus status;
-        uint64 votingRights;
     }
 
     uint256 startTime = now;
@@ -35,9 +39,10 @@ contract BurnFeePoll {
     /**
     * Events
     */
-    event VoteIssued(address voter, uint fee, uint votingRights);
-    event VoteChanged(address voter, uint fee, uint votingRights);
-    event VoteCancelled(address voter, uint votingRights);
+    event VoteIssued(address voter, uint fee);
+    event VoteChanged(address voter, uint fee);
+    event VoteCancelled(address voter);
+    event ParticipantStatus(address voter, bool status);
 
     /**
     * @dev constructor
@@ -45,37 +50,45 @@ contract BurnFeePoll {
     constructor() public {}
 
     /**
+    * @dev checkParticipant(): only for debugging purpose
+    */
+    function checkParticipant()
+    public
+    {
+        bool isParticipant = digixDaoContract.isParticipant(msg.sender);
+        emit ParticipantStatus(msg.sender, isParticipant);
+    }
+
+    /**
     * @dev issueVote(): instantiates a new vote
     * @param _fee voted fee
     */
     function issueVote (
-        uint memory _fee
+        uint _fee
     )
     public
     beforeDeadline()
-    hasValue()
     isQuarterParticipant()
-    hasNotVoted()
-    feeWithinRange()
+    hasStatus(VoteStatus.NOT_VOTED)
+    feeWithinRange(_fee)
     {
         votes[msg.sender] = Vote(_fee, VoteStatus.VOTED);
-        emit VoteIssued(msg.sender, _fee, votes[msg.sender].votingRights);
+        emit VoteIssued(msg.sender, _fee);
     }
 
     /**
     * @dev changeVote(): modifiy existing vote
     * @param _fee changed fee
     */
-    function changeVote(uint memory _fee)
+    function changeVote(uint _fee)
     public
     beforeDeadline()
-    hasValue()
     isQuarterParticipant()
     hasStatus(VoteStatus.VOTED)
-    feeWithinRange()
+    feeWithinRange(_fee)
     {
-        vote[msg.sender].fee = _fee;
-        emit VoteChanged(msg.sender, _fee, votes[msg.sender].votingRights);
+        votes[msg.sender].fee = _fee;
+        emit VoteChanged(msg.sender, _fee);
     }
 
     /**
@@ -87,44 +100,17 @@ contract BurnFeePoll {
     isQuarterParticipant()
     hasStatus(VoteStatus.VOTED)
     {
-        vote[msg.sender].status = VoteStatus.CANCELLED;
-        emit VoteCancelled(msg.sender, votes[msg.sender].votingRights);
-    }
-
-    /**
-    * @dev lockedDGDs(): get locked DGDs for participant
-    */
-    function lockedDGDs()
-    private
-    view
-    returns (uint256 _lockedDgd)
-    {
-        bytes memory payload = abi.encodeWithSignature("readUserInfo(address)", msg.sender);
-        (bool success, bytes memory returnData) = DGD_Q_CONTRACT.staticcall(payload);
-        if (success) {
-             _lockedDgd = returnData._lockedDgdStake;
-        } else {
-            _lockedDgd = 0;
-        }
+        votes[msg.sender].status = VoteStatus.CANCELLED;
+        emit VoteCancelled(msg.sender);
     }
 
     /**
     * Modifiers
     */
-    modifier hasNotVoted() {
-        require(vote[msg.sender]);
-        _;
-    }
-
     modifier beforeDeadline() {
         // @dev Voting deadline for the poll. No more vote changes alllowed after this.
         // @dev Hardcoded to: now + 90 days
-        require(startTime + 90*days > now);
-        _;
-    }
-
-    modifier hasValue() {
-        require(msg.value > 0);
+        require(startTime + now.mul(90 days) > now);
         _;
     }
 
@@ -142,9 +128,7 @@ contract BurnFeePoll {
     }
 
     modifier isQuarterParticipant() {
-        bytes memory payload = abi.encodeWithSignature("isParticipant(address)", msg.sender);
-        (bool success, bytes memory returnData) = DGD_Q_CONTRACT.staticcall(payload);
-        require(returnData._is);
+        require(digixDaoContract.isParticipant(msg.sender));
         _;
     }
 }
