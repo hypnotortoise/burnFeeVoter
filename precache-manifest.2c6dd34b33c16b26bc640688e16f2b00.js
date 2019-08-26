efc57708ce72fbc4799d62858a44dbf4fe4587c2
hypnotortoise <hypnotortoise@gmail.com>
2019-08-26 14:51:37 +0200

    Updates

---
+++ precache-manifest.2c6dd34b33c16b26bc640688e16f2b00.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bdd083698590a97c4a9cfe893c922326",
+    "url": "/burnFeeVoter/index.html"
+  },
+  {
+    "revision": "e6c72d4adba6b92980aa",
+    "url": "/burnFeeVoter/static/css/main.3e88a9a7.chunk.css"
+  },
+  {
+    "revision": "74e052fa92ced182f645",
+    "url": "/burnFeeVoter/static/js/2.47b242bc.chunk.js"
+  },
+  {
+    "revision": "e6c72d4adba6b92980aa",
+    "url": "/burnFeeVoter/static/js/main.3fe7b704.chunk.js"
+  },
+  {
+    "revision": "2bcbf3d78e25289a30e4",
+    "url": "/burnFeeVoter/static/js/runtime~main.e5b8f2cd.js"
+  }
+]);
